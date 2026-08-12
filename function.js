0b5584480cac168b6b9608df2048917a126d5e47
SPENCER BRIGHT <136099097+SpencerBrightest@users.noreply.github.com>
2026-08-12 05:47:44 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,43 +1,32 @@
-//function for even number
-function isEven(){
-  const Number =10;
-
-  if (Number%2==0){
-    console.log(`The number ${number} is even`)
-  }
-  else{
-    console.log(`The number ${Number} is not even`)
+// function for even number
+function isEven(number) {
+  if (number % 2 == 0) {
+    console.log(`The number ${number} is even`);
+  } else {
+    console.log(`The number ${number} is not even`);
   }
 }
+const userNumber = Number(prompt("Enter a number:"));
+isEven(userNumber);
 
-isEven();
-
-//function for comparing 3 numbers
-
-function largestNum(){
- const a=2;
- const b=3;
- const c=4;
-
-  if(a>b && a>c){
+// function for comparing 3 numbers
+function largestNum(a, b, c) {
+  if (a > b && a > c) {
     console.log(`${a} is the bigger number`);
-}
-  else if {b>a && b>c}{
-    console.log(`${b} is the gretter number`);
+  } else if (b > a && b > c) {
+    console.log(`${b} is the greater number`);
+  } else {
+    console.log(`${c} is the bigger number`);
   }
-
-else {
-  console.log(`${c} is the bigger number`);
 }
+const a = Number(prompt("Enter first number:"));
+const b = Number(prompt("Enter second number:"));
+const c = Number(prompt("Enter third number:"));
+largestNum(a, b, c);
 
-  largestNum();
-
-
-//chceking if number is a string
-
-  function reverseString(str){
-    return str.split('').reverse().join('');
-  }
-
-  console.log(reverseString("HelloSpencer"))
-  
+// function for reversing a string
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+const userString = prompt("Enter a string to reverse:");
+console.log(reverseString(userString));
